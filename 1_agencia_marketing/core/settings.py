c767e08aefb3709f42f616a7b01366ff30b21973
Daniel P <daptcas@gmail.com>
2024-03-24 22:39:18 -0500

    cambio de carpeta

--- 1_agencia_marketing/core/settings.py
+++ 1_agencia_marketing/core/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
